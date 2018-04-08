edfd3ad9c243b6fc82f757738e8a3ab094f7ec63
Ignas Bobinas <ignas.bobinas93@gmail.com>
2018-04-08 19:40:51 +0300

    Add public transaction data collection.

--- src/main/java/org/ignas/frauddetection/transactionstatistics/TransactionStatisticArchive.java
+++ src/main/java/org/ignas/frauddetection/transactionstatistics/TransactionStatisticArchive.java
@@ -18,52 +18,10 @@ import org.ignas.frauddetection.transactionstatistics.repositories.ConditionStor
 import org.ignas.frauddetection.transactionstatistics.repositories.GeneralTransactionsStorage;
 import org.joda.time.LocalDateTime;
 
+import static com.google.common.collect.Lists.newArrayList;
+
 public class TransactionStatisticArchive extends AbstractVerticle {
 
-    // TODO: delete after calculations implemented
-    private static final Statistics TEMPORARY_HARDCODED_RESULT = new Statistics(
-        new DebtorTransactionStatistics(
-            new Location(54.25123f, 25.45211f),
-            new Location(54.25123f, 25.45211f),
-            10f,
-            LocalDateTime.now().minusHours(1),
-            3600,
-            ImmutableList.of(
-                new PersonalPeriod(1,1f,2),
-                new PersonalPeriod(7,110f,4),
-                new PersonalPeriod(30,550f,22)
-            )
-        ),
-        new CredibilityScore(123f, 125f, 12f),
-        new CredibilityScore(123f, 125f, 12f),
-        new CredibilityScore(123f, 125f, 12f),
-        new PublicStatistics(
-            Lists.newArrayList(
-                new SumStatistics(1, 10f,10f, 11f, 6f),
-                new SumStatistics(7, 50f,20f, 55f, 11f),
-                new SumStatistics(30, 500f,100f, 505f, 120f)
-            ),
-            Lists.newArrayList(
-                new RatioStatistics(1, 10f,1f),
-                new RatioStatistics(7, 20f,5f),
-                new RatioStatistics(30, 50f,10f)
-            ),
-            Lists.newArrayList(
-                new CountStatistics(1, 1, 0, 2, 1),
-                new CountStatistics(7, 3, 0, 5, 1),
-                new CountStatistics(30, 20, 3, 22, 3)
-            ),
-            Lists.newArrayList(
-                new TimeDifferenceStatistics(1f, 2f)
-            ),
-            Lists.newArrayList(
-                new DistanceDifferenceStatistics(10f, 15f)
-            ),
-            Lists.newArrayList(
-                new DistanceDifferenceStatistics(10f, 15f)
-            )
-        )
-    );
 
     private GeneralTransactionsStorage generalTransactionsStorage;
 
@@ -110,6 +68,8 @@ public class TransactionStatisticArchive extends AbstractVerticle {
 
                         ExternalConditions conditions = event.result().resultAt(0);
 
+                        conditions.getCreditor().get
+                        new CredibilityScore();
 
                         NonPeriodicGeneralStats nonPeriodicStats = event.result().resultAt(1);
 
@@ -128,6 +88,44 @@ public class TransactionStatisticArchive extends AbstractVerticle {
 
                         DistanceDifferenceStatistics commonDiff = new DistanceDifferenceStatistics(averageDistanceCommon, deviationDistanceCommon);
 
+
+                        Statistics TEMPORARY_HARDCODED_RESULT = new Statistics(
+                            new DebtorTransactionStatistics(
+                                new Location(54.25123f, 25.45211f),
+                                new Location(54.25123f, 25.45211f),
+                                10f,
+                                LocalDateTime.now().minusHours(1),
+                                3600,
+                                ImmutableList.of(
+                                    new PersonalPeriod(1,1f,2),
+                                    new PersonalPeriod(7,110f,4),
+                                    new PersonalPeriod(30,550f,22)
+                                )
+                            ),
+                            new CredibilityScore(123f, 125f, 12f),
+                            new CredibilityScore(123f, 125f, 12f),
+                            new CredibilityScore(123f, 125f, 12f),
+                            new PublicStatistics(
+                                newArrayList(
+                                    new SumStatistics(1, 10f,10f, 11f, 6f),
+                                    new SumStatistics(7, 50f,20f, 55f, 11f),
+                                    new SumStatistics(30, 500f,100f, 505f, 120f)
+                                ),
+                                newArrayList(
+                                    new RatioStatistics(1, 10f,1f),
+                                    new RatioStatistics(7, 20f,5f),
+                                    new RatioStatistics(30, 50f,10f)
+                                ),
+                                newArrayList(
+                                    new CountStatistics(1, 1, 0, 2, 1),
+                                    new CountStatistics(7, 3, 0, 5, 1),
+                                    new CountStatistics(30, 20, 3, 22, 3)
+                                ),
+                                newArrayList(timeDiff),
+                                newArrayList(commonDiff),
+                                newArrayList(lastDiff)
+                            )
+                        );
 
                     }
                 );
